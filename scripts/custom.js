f1badac1d4ef4baba2014f1444517402b03772df
ashish-steg <akumar3@stegtechnologies.com>
2018-07-12 19:38:55 +0530

    Worked on PDP, Cart page, Cheackout page and Order confirmation for responsive

--- scripts/custom.js
+++ scripts/custom.js
@@ -6,9 +6,19 @@ define([
         	$(".search-icon").toggleClass("search-open");
             $(".mz-search-filed").toggle();
         });
-		$(".container-links h4").click(function() {
+        
+		$(".mz-mobile-tabs li").click(function() {
 			$(this).toggleClass("open");
-			$(this).next('ul').slideToggle();
-		});
+			$("ul").not($(this).next()).slideUp('medium');
+		});	
+
+  		$(".container-links ul:not(:first)").addClass("list");
+	    var allPanels = $('.container-links .list');
+	  	$('.container-links h4').click(function() {
+	    	allPanels.slideUp();
+	    	allPanels.prev().removeClass("open");
+	    	$(this).addClass("open").next().slideDown();
+	    	return false;
+	  	});
 	});
 }); 
