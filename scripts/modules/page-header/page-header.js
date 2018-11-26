92f8555d26973abbfb398013f48af273c27cfa21
sanjaySteg <ssingh1@stegtechnologies.com>
2018-11-26 10:39:04 +0530

    HAR-43 removed console

--- scripts/modules/page-header/page-header.js
+++ scripts/modules/page-header/page-header.js
@@ -9,10 +9,6 @@ define([
                 templateName: 'modules/page-header/threshold-Msg'          
             });
      api.request("GET", "/api/commerce/carts/current").then(function(res) {
-        console.log("test");
-       // console.log(res.discountThresholdMessages[0].message);
-        console.log(JSON.stringify(res));
-        console.log("test222");
             var product = new ProductModels.Product(res);
             var Quickview = new quickview({
             model: product,
