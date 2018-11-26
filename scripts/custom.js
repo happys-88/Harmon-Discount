bcc77b6edfd66dd433f6e68382e7814c78c69b9f
sanjaySteg <ssingh1@stegtechnologies.com>
2018-11-26 18:37:43 +0530

    Reset PAssword fix

--- scripts/custom.js
+++ scripts/custom.js
@@ -165,4 +165,17 @@ define([
 	    else expires = "";
 	    document.cookie = name+"="+value+expires+"; path=/";
 	}
+
+	$("#resetPassword").blur(function() {
+  		var newPassword = $('#resetPassword').val();
+  		var regularExpression  = /^[a-zA-Z0-9!@#$%^&*]{8}$/;
+  		if(!regularExpression.test(newPassword)) {
+  			$('.mz-reset-password-val').css("display","block");
+          $('.mz-reset-password-val').text('Minimum length should 8 and alphanumeric');
+        	return false;	
+   		 }else {
+   		 	$('.mz-reset-password-val').text('');
+   		 }
+  		
+	});
 }); 
