00f27add383051535dfb81e28db748f26ecfa7b7
Ravina <rrani@stegtechnologies.com>
2018-12-18 17:11:33 +0530

    fixed HAR-84

--- scripts/pages/myaccount.js
+++ scripts/pages/myaccount.js
@@ -378,6 +378,44 @@
             'editingCard.contactId',
             'editingContact.address.countryCode'
         ],
+        additionalEvents: {
+            "blur #mz-payment-credit-card-number": "changeCardType"
+        },
+        changeCardType: function(e) {
+            var number = e.target.value;
+            var cardType = '';
+            // visa
+            var re = new RegExp("^4");
+            if (number.match(re) !== null) {
+                cardType = "VISA";
+            }
+
+            // Mastercard 
+            // Updated for Mastercard 2017 BINs expansion
+            if (/^(5[1-5][0-9]{14}|2(22[1-9][0-9]{12}|2[3-9][0-9]{13}|[3-6][0-9]{14}|7[0-1][0-9]{13}|720[0-9]{12}))$/.test(number))
+                cardType = "MC";
+
+            // AMEX
+            re = new RegExp("^3[47]");
+            if (number.match(re) !== null)
+                cardType = "AMEX";
+
+            // Discover
+            re = new RegExp("^(6011|622(12[6-9]|1[3-9][0-9]|[2-8][0-9]{2}|9[0-1][0-9]|92[0-5]|64[4-9])|65)");
+            if (number.match(re) !== null)
+                cardType = "DISCOVER";
+
+            $('.mz-card-type-images').find('span').removeClass('active');
+            if (cardType) {
+                this.model.set('card.paymentOrCardType', cardType);
+                // $("#mz-payment-credit-card-type").val(cardType);
+                $('#mz-payment-credit-card-type').find('option[value="' +cardType+ '"]').prop("selected", true).change();
+                $('.mz-card-type-images').find('span[data-mz-card-type-image="' + cardType + '"]').addClass('active');
+            } else {
+                this.model.set('card.paymentOrCardType', null);
+            }
+            
+        },
         beginEditCard: function (e) {
             var id = this.editing.card = e.currentTarget.getAttribute('data-mz-card');
             this.model.beginEditCard(id);
