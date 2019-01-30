c599f7f531c8f59c304f6148e498266b4a411691
ashish-steg <akumar3@stegtechnologies.com>
2019-01-30 11:10:16 +0530

    Worked on HAR-183

--- scripts/widgets/home-slider.js
+++ scripts/widgets/home-slider.js
@@ -3,23 +3,23 @@ require([
     'bxslider'
 ], function ($, bxSlider) {
     $('#mz-home-slider .slider').ready(function () {
-            $('#mz-home-slider .slider').bxSlider({
-                auto: false,
-                useCSS: false,
-                speed: 1000,
-                minSlides: 1,
-                maxSlides: 1,
-                moveSlides: 1,
-                slideMargin: 0,
-                infiniteLoop: false,
-                controls: true,
-                pager: true,
-                hideControlOnEnd: true,
-                touchEnabled: true,
-                adaptiveHeight: true,
-                onSliderLoad: function () {
-                    $(".slider").css("visibility", "visible");
-                }
-            });
+        $('#mz-home-slider .slider').bxSlider({
+            auto: false,
+            useCSS: false,
+            speed: 1000,
+            minSlides: 1,
+            maxSlides: 1,
+            moveSlides: 1,
+            slideMargin: 0,
+            infiniteLoop: true,
+            controls: true,
+            pager: true,
+            hideControlOnEnd: true,
+            touchEnabled: true,
+            adaptiveHeight: true,
+            onSliderLoad: function () {
+                $(".slider").css("visibility", "visible");
+            }
         });
+    });
 });
